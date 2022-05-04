89bf9b31c0789eecf2ab92b2d896b3f5df125d16
Piotr Pabich <235944@edu.p.lodz.pl>
2022-05-04 09:51:31 +0200

    Zmiany 1.5 Testy, diagramy, dokumentacja

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,7 +1,23 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa DefaultCountingOutRhymer:
+ * główna klasa implementująca najważniejsze metody oraz przechowująca zmienne
+ * takie jak wielkości tablicy oraz znajdujące się w niej liczby
+ * @author Piotr Pabich 235944
+ *
+ */
 public class DefaultCountingOutRhymer {
 	
+	/**
+	 * Stałe i zmienne:
+	 * EMPTY_RHYMER_ARRAY   - błąd, gdy tablica jest pusta
+	 * DEFAULT_RETURN_VALUE - błąd w metodach peekaboo() i countOut() 
+	 * RHYMER_SIZE          - wielkość tablicy
+	 * FULL_RHYMER_ARRAY    - ostatni indeks w tablicy
+	 * numbers 				- tablica przechowująca liczby
+	 * total   				- obecna wielkość tablicy
+	 */
 	private static final int EMPTY_RHYMER_ARRAY = -1;
 	
 	private static final int DEFAULT_RETURN_VALUE = -1;
@@ -14,33 +30,63 @@ public class DefaultCountingOutRhymer {
 
 	private int total = EMPTY_RHYMER_ARRAY;
 
+	/**
+	 * getTotal() - getter zwracający pole total
+	 * @return zwraca pole prywatne total
+	 */
 	public int getTotal(){
 		return this.total;
 	}
 
+	/**
+	 * setTotal(int total) - setter ustawiajacy pole total
+	 * @param total - zmienna ustawiana w pole prywatne total
+	 */
 	public void setTotal(int total){
 		this.total = total;
 	}
 
+	/**
+	 * countIn(int in) - metoda dodająca element do tablicy
+	 * @param in - element dodawany do tablicy numbers
+	 */
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
+	/**
+	 * callCheck() - metoda sprawdzająca czy tablica jest pusta
+	 * @return zwraca true lub false
+	 */
 	public boolean callCheck() {
 		return getTotal() == EMPTY_RHYMER_ARRAY;
 	}
 
+	/**
+	 * isFull() - metoda sprawdzająca czy osiągnieto 
+     * 			  maksymalną ilość elementów w tablicy
+	 * @return zwraca true lub false
+	 */
 	public boolean isFull() {
 		return getTotal() == FULL_RHYMER_ARRAY;
 	}
 
+	/**
+	 * peekaboo()  - metoda zwracająca ostatnią wartość z tablicy
+	 * @return zwraca element pod indeksem total lub błąd DEFAULT_RETURN_VALUE
+	 */
 	protected int peekaboo() {
 		if (callCheck())
 			return DEFAULT_RETURN_VALUE;
 		return numbers[getTotal()];
 	}
 
+	/**
+	 * countOut()  - metoda zwracająca wartość z tablicy pod indeksem total,
+	 * 				 a pózniej dekrementującą wartość total
+	 * @return zwraca element pod indeksem total lub błąd DEFAULT_RETURN_VALUE
+	 */
 	public int countOut() {
 		if (callCheck())
 			return DEFAULT_RETURN_VALUE;
--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -7,9 +7,15 @@ import edu.kis.vh.nursery.factory.RhymersFactory;
 
 class RhymersDemo {
 
+	
 	public static void main(String[] args) {
 		RhymersFactory factory = new DefaultRhymersFactory();
 		
+		testRhymers(factory);
+		
+	}
+
+	private static void testRhymers(RhymersFactory factory) {
 		DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
 				factory.getFIFORhymer(), factory.getHanoiRhymer()};
 		
@@ -29,7 +35,6 @@ class RhymersDemo {
 		
 		System.out.println("total rejected is "
 				+ ((HanoiRhymer) rhymers[3]).reportRejected());
-		
 	}
 	
 }
